0e4b7410224df27b53ee17d77dd1d9c2a089ba51
Nuku Sape <snyao@aol.com>
2014-08-24 23:03:33 -0400

    Add order history page

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,8 @@ class Order < ActiveRecord::Base
 	validates :address, :city, :state, presence: true
 
 	belongs_to :listing
-	belongs_to :buyer, class_name: "user"
-	belongs_to :seller, class_name: "user"
+	belongs_to :buyer, class_name: "User"
+	belongs_to :seller, class_name: "User"
 	has_many :sales, class_name: "Order", foreign_key: "seller_id"
 	has_many :purchases, class_name: "Order", foreign_key: "buyer_id"
 end
